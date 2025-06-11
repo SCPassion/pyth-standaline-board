cc6b609ccbe39a2d7af8e920a0ed1b6d7f5f9126
SCPassion <bernard.cryptohk@gmail.com>
2025-06-11 21:46:58 +0200

    rename

--- action/pythActions.ts
+++ action/pythActions.ts
@@ -4,7 +4,7 @@ import type {
   MyPublisherInfo,
   PythGeneralStats,
   PythStakingInfo,
-} from "@/types/types";
+} from "@/types/pythTypes";
 import {
   extractPublisherData,
   PythStakingClient,
